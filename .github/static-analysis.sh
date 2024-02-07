3776ec6fddef2d81a4c713caa205449e6d0b2451
Keith Maxwell <keith.maxwell@gmail.com>
2024-02-07 22:24:30 +0000

    Refactor static analysis workflow

---
+++ .github/static-analysis.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# SPDX-FileCopyrightText: 2024 Keith Maxwell <keith.maxwell@gmail.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+
+yamllint . \
+&& git ls-files '*.yaml' '*.yml' | xargs npm exec prettier -- --check \
+&& git ls-files '*.md' \
+| xargs npm exec prettier -- --check --prose-wrap always \
+&& git ls-files '*.json' | xargs -I{} sh -c "jq . {} | cmp {} -"
